cc8a7ee45c4aa8556d8624f98ca374820ca27fe5
Roger Lopesierra Olivera <rogerlopesierraolivera@gmail.com>
2022-12-08 14:09:12 -0500

    heroesApp funcional sin proteccion rutas

---
+++ src/heroes/components/HeroCard.jsx
@@ -0,0 +1,56 @@
+import { Link } from 'react-router-dom';
+
+const CharactersByHero = ({ alter_ego, characters }) => {
+	/* if ( alter_ego === characters ) return (<></>);
+	return <p>{characters}</p> */
+	return ( alter_ego === characters )
+	? <></>
+	: <p>{characters}</p>
+}
+
+export const HeroCard = ({ 
+  id,
+	superhero,
+	publisher,
+	alter_ego,
+	first_appearance,
+	characters,
+}) => {
+
+	const heroImageUrl = `/assets/heroes/${ id }.jpg`;
+
+  return (
+    <div className="col animate__animated animate__fadeIn">
+			<div className="card">
+
+				<div className="row no-gutters">
+					<div className="col-4">
+						<img src={ heroImageUrl } alt={superhero} className="card-img" />
+					</div>
+
+					<div className="col-8">
+						<div className="card-body">
+							<div className="card-title">{superhero}</div>
+							<p>{alter_ego}</p>
+
+							{/* {
+								( alter_ego !== characters ) && ( <p>{characters}</p> )
+							} */}
+							<CharactersByHero characters={characters} alter_ego={alter_ego}></CharactersByHero>
+							
+							<p className="card-text">
+								<small className="text-muted">{first_appearance}</small>
+							</p>
+
+							<Link to={`/hero/${ id }`}>
+								mas...
+							</Link>
+
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</div>
+  )
+}
---
+++ src/heroes/index.js
@@ -0,0 +1,5 @@
+
+export * from './pages';
+export * from './helpers';
+export * from './components';
+export * from './routes/HeroesRoutes';
